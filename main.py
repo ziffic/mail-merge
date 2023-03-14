08db9b8bfe0ba70f3ab0d19e1947fdd87670ab63
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-14 16:43:38 -0700

    refactored

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-with open("Input/Names/invited_names.txt") as names_file:
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
 
-with open("Input/Letters/starting_letter.txt") as file:
-    letter = file.read()
-
-for name in names:
-    current_name = str(name.replace("\n", ""))
-    new_letter = str(letter.replace("[name]", current_name))
-    with open(f"Output/ReadyToSend/letter_to_{current_name}.txt", mode="w") as data:
-        data.write(new_letter)
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
